de896f3c0e4dcb509660c842e437140205b0c58e
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2025-01-03 20:36:52 -0800

    added Express 5 routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from "express"
+import userRouter from "./routes/user.route.js"
+import postRouter from "./routes/post.route.js"
+import commentRouter from "./routes/comment.route.js"
 
 const app = express()
 
-console.log(process.env.test)
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+
 
 app.listen(3000,()=>{
     console.log("Server is running!")
