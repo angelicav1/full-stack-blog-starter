84785c71a0c8031ea3399351463be1ca9d393397
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2025-01-05 17:03:58 -0800

    added MERN stack CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,22 @@ import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
 
 
-const app = express()
+const app = express();
+app.use(express.json())
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+
+    res.json({
+        message:error.message || "Something went wrong!",
+        status:error.status,
+        stack: error.stack,
+    })
+})
 
 
 app.listen(3000,()=>{
